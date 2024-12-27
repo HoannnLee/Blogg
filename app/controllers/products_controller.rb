7b1524aea7b39ddae2d0264a921f8587da9f28cb
HoannnLee <Leecghoan@gmail.com>
2024-12-27 15:38:12 +0700

    add code to github

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,61 @@
+# class ProductsController < ApplicationController
+#   def index
+#   end
+# end
+
+class ProductsController < ApplicationController
+
+  # allow_unauthenticated_access only: %i[ index show ]
+
+  before_action :set_product, only: %i[ show edit update destroy ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    # byebug 
+    # @product = Product.find_by(id: params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def set_product
+        @product = Product.find_by(id: params[:id])
+    end
+
+    def product_params
+      byebug
+      params.require(:product).permit(:name, :des, :price, :description ,:featured_image,
+      :inventory_count)
+    end
+
+end
