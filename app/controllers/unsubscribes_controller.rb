31be86e792e941ac239db9ca821591496fe3440d
HoannnLee <Leecghoan@gmail.com>
2025-01-01 21:38:54 +0700

    up base css

---
+++ app/controllers/unsubscribes_controller.rb
@@ -0,0 +1,16 @@
+class UnsubscribesController < ApplicationController
+    allow_unauthenticated_access
+    before_action :set_subscriber
+  
+    def show
+      @subscriber&.destroy
+      redirect_to root_path, notice: "Hủy đăng ký thành công"
+    end
+  
+    private
+  
+    def set_subscriber
+      @subscriber = Subscriber.find_by_token_for(:unsubscribe, params[:token])
+    end
+  end
+  
